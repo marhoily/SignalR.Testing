b99ac50b364cbf35a3d107a6994f2a52770caeee
Ilya Margolin <imargolin@luware.net>
2015-09-09 19:54:58 +0300

    Simplify

--- SignalR.Tests.Common/Infrastructure/MemoryTestHost.cs
+++ SignalR.Tests.Common/Infrastructure/MemoryTestHost.cs
@@ -1,36 +1,100 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Client.Transports;
+using Microsoft.AspNet.SignalR.Configuration;
+using Microsoft.AspNet.SignalR.Messaging;
+using Microsoft.AspNet.SignalR.Tracing;
 using Owin;
 
 namespace SignalR.Tests.Common
 {
-    public class MemoryTestHost : TracingTestHost
+    public class MemoryTestHost : ITestHost
     {
+        #region MyRegion
+                private static readonly string[] TraceSources =
+        {
+            "SignalR.Transports.WebSocketTransport",
+            "SignalR.Transports.ServerSentEventsTransport",
+            "SignalR.Transports.ForeverFrameTransport",
+            "SignalR.Transports.LongPollingTransport",
+            "SignalR.Transports.TransportHeartBeat"
+        };
+
+        private readonly TextWriterTraceListener _listener;
+        private ITraceManager _traceManager;
+
+        protected MemoryTestHost(string logPath)
+        {
+            _listener = new TextWriterTraceListener(logPath + ".transports.log");
+            Disposables = new List<IDisposable>();
+            ExtraData = new Dictionary<string, string>();
+        }
+
+        public IClientTransport Transport { get; set; }
+
+        public IList<IDisposable> Disposables { get; private set; }
+
+        public IDictionary<string, string> ExtraData { get; private set; }
+
+        public Func<IClientTransport> TransportFactory { get; set; }
+
+        private IDependencyResolver Resolver { get; set; }
+
+        #endregion
         private readonly MemoryHost _host;
 
         public MemoryTestHost(MemoryHost host)
-            : base("")
+            : this("")
         {
             _host = host;
         }
 
-        public override string Url
+        public string Url
         {
             get { return "http://memoryhost"; }
         }
 
-        public override void Initialize(int? keepAlive = -1,
+        public void Initialize(int? keepAlive = -1,
             int? connectionTimeout = 110,
             int? disconnectTimeout = 30,
             int? transportConnectTimeout = 5,
             int? maxIncomingWebSocketMessageSize = 64*1024,
             bool enableAutoRejoiningGroups = false)
         {
-            base.Initialize(keepAlive,
-                connectionTimeout,
-                disconnectTimeout,
-                transportConnectTimeout,
-                maxIncomingWebSocketMessageSize,
-                enableAutoRejoiningGroups);
+            Resolver = Resolver ?? new DefaultDependencyResolver();
+
+            _traceManager = Resolver.Resolve<ITraceManager>();
+            _traceManager.Switch.Level = SourceLevels.Verbose;
+
+            foreach (var sourceName in TraceSources)
+            {
+                var source = _traceManager[sourceName];
+                source.Listeners.Add(_listener);
+            }
+
+            var configuration = Resolver.Resolve<IConfigurationManager>();
+
+            if (connectionTimeout != null)
+            {
+                configuration.ConnectionTimeout = TimeSpan.FromSeconds(connectionTimeout.Value);
+            }
+
+            if (disconnectTimeout != null)
+            {
+                configuration.DisconnectTimeout = TimeSpan.FromSeconds(disconnectTimeout.Value);
+            }
+
+            if (transportConnectTimeout != null)
+            {
+                configuration.TransportConnectTimeout = TimeSpan.FromSeconds(transportConnectTimeout.Value);
+            }
+
+            configuration.MaxIncomingWebSocketMessageSize = maxIncomingWebSocketMessageSize;
+
+            var bus = new FakeScaleoutBus(Resolver);
+            Resolver.Register(typeof (IMessageBus), () => bus);
 
             _host.Configure(app =>
             {
@@ -41,11 +105,23 @@ namespace SignalR.Tests.Common
             });
         }
 
-        public override void Dispose()
+        public void Dispose()
         {
             _host.Dispose();
 
-            base.Dispose();
+            _listener.Flush();
+
+            foreach (var sourceName in TraceSources)
+            {
+                _traceManager[sourceName].Listeners.Remove(_listener);
+            }
+
+            _listener.Dispose();
+
+            foreach (var d in Disposables)
+            {
+                d.Dispose();
+            }
         }
     }
 }
