476e308c3276cc8cee15d67bad8d0f1aead63e7a
Ilya Margolin <imargolin@luware.net>
2015-09-09 20:04:41 +0300

    Simplify

--- SignalR.Tests.Common/Infrastructure/MemoryTestHost.cs
+++ SignalR.Tests.Common/Infrastructure/MemoryTestHost.cs
@@ -1,34 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Client.Transports;
-using Microsoft.AspNet.SignalR.Configuration;
 using Microsoft.AspNet.SignalR.Messaging;
-using Microsoft.AspNet.SignalR.Tracing;
 using Owin;
 
 namespace SignalR.Tests.Common
 {
     public class MemoryTestHost : ITestHost
     {
-        private static readonly string[] TraceSources =
-        {
-            "SignalR.Transports.WebSocketTransport",
-            "SignalR.Transports.ServerSentEventsTransport",
-            "SignalR.Transports.ForeverFrameTransport",
-            "SignalR.Transports.LongPollingTransport",
-            "SignalR.Transports.TransportHeartBeat"
-        };
-
         private readonly MemoryHost _host;
-        private readonly TextWriterTraceListener _listener;
-        private ITraceManager _traceManager;
 
         public MemoryTestHost(MemoryHost host)
-
         {
-            _listener = new TextWriterTraceListener(".transports.log");
             Disposables = new List<IDisposable>();
             _host = host;
         }
@@ -37,20 +21,24 @@ namespace SignalR.Tests.Common
         public Func<IClientTransport> TransportFactory { get; set; }
         public IClientTransport Transport { get; set; }
 
-        public string Url { get { return "http://memoryhost"; } }
+        public string Url
+        {
+            get { return "http://memoryhost"; }
+        }
+
+        public void Dispose()
+        {
+            _host.Dispose();
+
+            foreach (var d in Disposables)
+            {
+                d.Dispose();
+            }
+        }
 
         public void Initialize()
         {
             var resolver = new DefaultDependencyResolver();
-
-            _traceManager = resolver.Resolve<ITraceManager>();
-            _traceManager.Switch.Level = SourceLevels.Verbose;
-
-            foreach (var sourceName in TraceSources)
-            {
-                var source = _traceManager[sourceName];
-                source.Listeners.Add(_listener);
-            }
 
             var bus = new FakeScaleoutBus(resolver);
             resolver.Register(typeof (IMessageBus), () => bus);
@@ -62,25 +50,6 @@ namespace SignalR.Tests.Common
                     EnableDetailedErrors = true
                 });
             });
-        }
-
-        public void Dispose()
-        {
-            _host.Dispose();
-
-            _listener.Flush();
-
-            foreach (var sourceName in TraceSources)
-            {
-                _traceManager[sourceName].Listeners.Remove(_listener);
-            }
-
-            _listener.Dispose();
-
-            foreach (var d in Disposables)
-            {
-                d.Dispose();
-            }
         }
     }
 }
